018d5622a63ae2198c0ed43905c28b94e75d4133
Gustavo Gaspar <gustavominghini@gmail.com>
2022-07-04 14:24:04 -0300

    Primeiro Commit

---
+++ src/main/java/TFT_Cup_BackEnd/TftCupBackEndApplication.java
@@ -0,0 +1,13 @@
+package TFT_Cup_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftCupBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TftCupBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TFT_Cup_BackEnd/TftCupBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package TFT_Cup_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftCupBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
